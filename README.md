e08d867e787b75aca68a44d181f43d9d3559a373
Joseph Nguyen <vietjew@gmail.com>
2014-10-25 18:07:42 -0700

    Update readme and add gitignore

--- README.md
+++ README.md
@@ -2,4 +2,6 @@ Coding Challenge Solutions
 ================
 
 This repo contains my solutions to coding challenges on CodeEval, Coderbyte, and
-other such sites. Problems are stated in comments of each file.
+other such sites. 
+
+Problem descriptions are stated included as comments at the top of each file.
---
+++ multiples.js
@@ -0,0 +1,35 @@
+// Given numbers x and n, where n is a power of 2, print out the smallest multiple of n which is greater than or equal to x. Do not use division or modulo operator.
+//
+// INPUT SAMPLE:
+//
+// The first argument will be a path to a filename containing a comma separated list of two integers, one list per line. E.g.
+//
+// 13,8
+// 17,16
+// OUTPUT SAMPLE:
+//
+// Print to stdout, the smallest multiple of n which is greater than or equal to x, one per line. E.g.
+//
+// 16
+// 32
+
+// var fs  = require("fs");
+
+var fs = "13,8\n17,16"
+
+// fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
+fs.toString().split('\n').forEach(function (line) {
+	    if (line != "") {
+        var nums = line.split(',')
+				nums.forEach(function (num, ind, arr) {
+					arr[ind] = parseInt(num);
+				});
+				a = nums[0];
+				b = nums[1];
+				for (var i = b; i < a; i *= 2) {
+					b = i;
+				}
+				console.log(b);
+    }
+});
+
